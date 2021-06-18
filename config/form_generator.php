d4d486d3c1c9488dd1252b21b1ee85446806c2fb
agus24 <agusx244@gmail.com>
2021-06-18 17:03:50 +0700

    add basic packages

---
+++ config/form_generator.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /**
+     * Set your default styling 
+     * the option is [tailwind, bootstrap]
+     */
+    "style" => "tailwind",
+
+    /**
+     * Fill this with your view path
+     */
+    "form_template" => null
+];
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::truncate();
+        Role::create(['name' => 'superuser']);
+        Role::create(['name' => 'employee']);
+    }
+}
