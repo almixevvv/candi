bb7bce20a6fa87719452adb72fd2680aa2b5bbe4
agus24 <agusx244@gmail.com>
2021-07-10 04:10:00 +0700

    update

--- database/migrations/2021_07_09_174531_modify_profile_setting_table_change_youtube_field_to_twitter.php
+++ database/migrations/2021_07_09_174531_modify_profile_setting_table_change_youtube_field_to_twitter.php
@@ -15,7 +15,7 @@ class ModifyProfileSettingTableChangeYoutubeFieldToTwitter extends Migration
     {
         Schema::table('profile_settings', function (Blueprint $table) {
             $table->dropColumn('youtube');
-            $table->string('twitter')->nullable();
+            $table->string('twitter')->default('');
         });
     }
 
@@ -27,7 +27,7 @@ class ModifyProfileSettingTableChangeYoutubeFieldToTwitter extends Migration
     public function down()
     {
         Schema::table('profile_settings', function (Blueprint $table) {
-            $table->string('youtube')->nullable();
+            $table->string('youtube')->default('');
             $table->dropColumn('twitter');
         });
     }
