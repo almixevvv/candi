3061b0c076f509fc4b3853494a3f70c48aedd754
agus24 <agusx244@gmail.com>
2021-07-10 23:35:30 +0700

    create directory category component

---
+++ resources/views/components/directory-categories.blade.php
@@ -0,0 +1,49 @@
+<div class="destinations listing-categories">
+    <div class="wrap">
+        <div class="wrap_float">
+            <div class="section_content">
+                <div class="box_image">
+                    <a href="/home" class="logo">
+                        <img src="/images/icon-restaurant.svg" alt="">
+                    </a>
+                    <label>Restaurant</label>
+                </div>
+
+                <div class="box_image">
+                    <a href="/'home" class="logo">
+                        <img src="/images/icon-cafe.svg" alt="">
+                    </a>
+                    <label>Cafe</label>
+                </div>
+
+                <div class="box_image">
+                    <a href="/home" class="logo">
+                        <img src="/images/icon-bar.svg" alt="">
+                    </a>
+                    <label>Bar</label>
+                </div>
+
+                <div class="box_image2">
+                    <a href="/home" class="logo">
+                        <img src="/images/icon-tour.svg" alt="">
+                    </a>
+                    <label>Tour Attraction</label>
+                </div>
+
+                <div class="box_image">
+                    <a href="/home" class="logo">
+                        <img src="/images/icon-hotel.svg" alt="">
+                    </a>
+                    <label>Hotel & Resort</label>
+                </div>
+
+                <div class="box_image">
+                    <a href="/home" class="logo">
+                        <img src="/images/icon-shopping.svg" alt="">
+                    </a>
+                    <label>Shopping Center</label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
