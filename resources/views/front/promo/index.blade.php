7c12589e63ed82de880832fb8756a09ce947fd4a
Gustiawan Ouwawi <agusx244@gmail.com>
2021-07-10 23:42:56 +0700

    apply directory categories component (#43)

--- resources/views/front/promo/index.blade.php
+++ resources/views/front/promo/index.blade.php
@@ -10,55 +10,7 @@
             </div>
         </div>
     </div>
-    <div class="destinations">
-        <div class="wrap">
-            <div class="wrap_float">
-                <div class="section_content" style="padding-left:10%;padding-right:5%;">
-                    <div class="box_image">
-                        <a href="/home" class="logo">
-                            <img src="/images/icon-restaurant.svg" alt="" >
-                        </a>
-                        <label >Restaurant</label>
-                    </div>
-                    
-                    <div class="box_image">
-                        <a href="/'home" class="logo">
-                            <img src="/images/icon-cafe.svg" alt="" >
-                        </a>
-                        <label >Cafe</label>
-                    </div>
-                    
-                    <div class="box_image">
-                        <a href="/home" class="logo">
-                            <img src="/images/icon-bar.svg" alt="" >
-                        </a>
-                        <label >Bar</label>
-                    </div>
-                            
-                    <div class="box_image2" >
-                        <a href="/home" class="logo">
-                            <img src="/images/icon-tour.svg" alt="" >
-                        </a>
-                        <label >Tour Attraction</label>
-                    </div>
-                    
-                    <div class="box_image">
-                        <a href="/home" class="logo">
-                            <img src="/images/icon-hotel.svg" alt="" >
-                        </a>
-                        <label >Hotel & Resort</label>
-                    </div>
-                    
-                    <div class="box_image">
-                        <a href="/home" class="logo">
-                            <img src="/images/icon-shopping.svg" alt="" >
-                        </a>
-                        <label >Shopping Center</label>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-directory-categories />
     <div class="page contacts-page full-width">
         <div class="wrap">
             <div class="wrap_float">
